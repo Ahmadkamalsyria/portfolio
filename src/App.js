fa79a3023437c350473cd9ded8a5802601a7bcea
Ahmad Kamal <Ahmad106107@gmail.com>
2018-07-11 16:32:23 +0200

    adding the templates

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Testimonials from './components/Testimonials';
+import Resume from './components/Resume';
+import About from './components/About';
+import Contact from './components/Contact';
+import Portfolio from './components/Portfolio';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header/>
+        <About/>
+        <Resume/>
+        <Portfolio />
+        <Testimonials/>
+        <Contact/>
+        
+        <Footer/>
       </div>
     );
   }
