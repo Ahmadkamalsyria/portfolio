9b2ea6efdec290ffe77498017ff9921ae7bf259b
Ahmad Kamal <Ahmad106107@gmail.com>
2018-07-16 23:30:41 +0200

    done with Header and About

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import $ from 'jquery';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Testimonials from './components/Testimonials';
@@ -9,11 +10,36 @@ import Contact from './components/Contact';
 import Portfolio from './components/Portfolio';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      foo : 'Ahmad',
+      Data : {}
+    }
+  }
+  getData(){
+    $.ajax({
+      url: 'http://localhost:3000/Data.json',
+      dataType: 'json',
+      success : function(data){
+        this.setState({Data : data})
+      }.bind(this),
+      error : function(xhr, status, err){
+        console.log(err);
+        alert(err);
+      }
+    })
+  }
+
+  componentDidMount(){
+    this.getData();
+  }
   render() {
+    console.log(this.state.Data.main);
     return (
       <div className="App">
-        <Header/>
-        <About/>
+        <Header data = {this.state.Data.main} />
+        <About data = {this.state.Data.main} />
         <Resume/>
         <Portfolio />
         <Testimonials/>
